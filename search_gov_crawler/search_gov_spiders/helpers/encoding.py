461f976418f69da4caeee9fccd48b69f9b64cbb1
Daniel Self <self.danielj@gmail.com>
2025-02-25 19:32:10 -0500

    SRCH-5855 support VISCII encoding

--- search_gov_crawler/search_gov_spiders/helpers/encoding.py
+++ search_gov_crawler/search_gov_spiders/helpers/encoding.py
@@ -1,28 +1,34 @@
+import logging
+import os
 
 import cchardet  # Faster and more reliable character encoding detection than chardet
-import logging
 
 # Needs muting to avoid its debug mode
-logging.getLogger("cchardet").addHandler(logging.NullHandler())
+logging.getLogger("cchardet").setLevel("INFO")
+
 
 def detect_encoding(data: bytes) -> str | None:
     """Detect the encoding of the given byte string."""
     detection_result = cchardet.detect(data)
     encoding = detection_result.get("encoding")
-    
+
+    # VISCII is not fully supported by python but can use cp1258
+    if str(encoding).upper() == "VISCII":
+        return "cp1258"
+
     return encoding if encoding else None
+
 
 def decode_http_response(response_bytes: bytes) -> str:
     """Decode an HTTP response, using the detected encoding or the response's default encoding."""
     try:
-        decoded_str = response_bytes.decode("utf-8")
-        return decoded_str
+        decoded_response = response_bytes.decode("utf-8")
     except UnicodeDecodeError:
-        pass
+        detected_encoding = detect_encoding(response_bytes)
 
-    detected_encoding = detect_encoding(response_bytes)
-    
-    try:
-        return response_bytes.decode(detected_encoding)
-    except (UnicodeDecodeError, TypeError):
-        return str(response_bytes)
+        try:
+            decoded_response = response_bytes.decode(detected_encoding)
+        except (UnicodeDecodeError, TypeError):
+            decoded_response = str(response_bytes)
+
+    return decoded_response
