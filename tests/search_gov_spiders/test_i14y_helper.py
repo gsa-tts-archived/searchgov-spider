05c1d2cdfc9341daa9d1de2e9ab0c315a54ae190
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2025-03-13 13:30:24 -0400

    Added tests for separate_file_name

--- tests/search_gov_spiders/test_i14y_helper.py
+++ tests/search_gov_spiders/test_i14y_helper.py
@@ -1,5 +1,6 @@
-from search_gov_crawler.elasticsearch.i14y_helper import null_date, detect_lang
+from search_gov_crawler.elasticsearch.i14y_helper import null_date, detect_lang, separate_file_name
 
+# Tests for null_date
 def test_null_date_valid_date():
     assert null_date("2025-03-13") == "2025-03-13"
 
@@ -15,6 +16,7 @@ def test_null_date_zero():
 def test_null_date_false():
     assert null_date(False) is None
 
+# Tests for detect_lang
 def test_detect_lang_english():
     assert detect_lang("This is a test sentence in English. And, this is another test sentence in English.") == "en"
 
@@ -32,3 +34,37 @@ def test_detect_lang_empty_text():
 
 def test_detect_lang_unrecognizable_text():
     assert detect_lang("1234567890!@#$%^&*()") is None
+
+# Tests for separate_file_name
+def test_separate_file_name_camel_case():
+    assert separate_file_name("camelCaseFile.pdf") == "camel Case File"
+
+def test_separate_file_name_snake_case():
+    assert separate_file_name("snake_case_file.pdf") == "snake case file"
+
+def test_separate_file_name_kebab_case():
+    assert separate_file_name("kebab-case-file.pdf") == "kebab case file"
+
+def test_separate_file_name_pascal_case():
+    assert separate_file_name("PascalCaseFile.pdf") == "Pascal Case File"
+
+def test_separate_file_name_mixed_case():
+    assert separate_file_name("mixedCase123File.pdf") == "mixed Case 123 File"
+
+def test_separate_file_name_numbers():
+    assert separate_file_name("file123Test.pdf") == "file 123 Test"
+
+def test_separate_file_name_symbols():
+    assert separate_file_name("file_with-symbols+test,file~name%test.pdf") == "file with symbols test file name test tar"
+
+def test_separate_file_name_no_extension():
+    assert separate_file_name("noExtensionFile") == "no Extension File"
+
+def test_separate_file_name_multiple_dots():
+    assert separate_file_name("file.with.multiple.dots.pdf") == "file with multiple dots"
+
+def test_separate_file_name_empty():
+    assert separate_file_name("") == ""
+
+def test_separate_file_name_only_extension():
+    assert separate_file_name(".pdf") == ""
