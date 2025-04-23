c70a2b2482de77524bac8f7a26eac9b0e17f81db
Daniel Self <self.danielj@gmail.com>
2025-04-23 12:20:37 -0400

    SRCH-5940 add additional capabilities

--- scripts/delete_by_domain.sh
+++ scripts/delete_by_domain.sh
@@ -33,22 +33,26 @@ query="{ \"query\": { \"term\": { \"domain_name\": { \"value\": \"${domain_name}
 count_response=$(curl -s -k -u $ES_USER:$ES_PASSWORD -X POST "$es_url/_count" -H 'Content-Type: application/json' -d "$query")
 if echo $count_response | grep -q '"count":[0-9]\+'; then
         result_count=`echo $count_response | grep -o '"count":[0-9]\+' | grep -o '[0-9]\+'`
+        if [ $result_count -eq 0 ]; then
+            echo "No records found for $domain_name. Exiting."
+            return 0
+        fi
+
         echo "Found $result_count records for $domain_name"
         echo "Response: $count_response"
-
         read -p "Are you sure you want to delete these records? " -n 1 -r
+
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             delete_response=$(curl -s -k -u $ES_USER:$ES_PASSWORD -X POST "$es_url/_delete_by_query" -H 'Content-Type: application/json' -d "$query")
             if echo $delete_response | grep -q '"deleted":[0-9]\+'; then
                 deleted_count=$(echo "$response" | grep -o '"deleted":[0-9]\+' | grep -o '[0-9]\+')
                 echo "Successfully deleted $result_count records for $domain_name"
                 echo "Response: $delete_response"
-            else:
+            else
                 echo "Failed to delete records for $domain_name."
                 echo "Response: $delete_response"
             fi
         fi
-
 else
         echo "Failed to count records for $domain_name"
         echo "Response: $count_response"
