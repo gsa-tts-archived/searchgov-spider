f0cca746574feccc479cf315bfa2857f59fb5b6b
Daniel Self <self.danielj@gmail.com>
2025-04-29 10:22:14 -0400

    SRCH-5988 update spidermond settings

--- search_gov_crawler/search_gov_spiders/monitors.py
+++ search_gov_crawler/search_gov_spiders/monitors.py
@@ -1,42 +1,48 @@
 from pathlib import Path
+from typing import ClassVar
+
 from spidermon import MonitorSuite
 from spidermon.contrib.actions.email.ses import SendSESEmail
 from spidermon.contrib.actions.reports.files import CreateFileReport
 from spidermon.contrib.scrapy.monitors.monitors import (
+    FinishReasonMonitor,
     ItemCountMonitor,
-    UnwantedHTTPCodesMonitor,
-    PeriodicItemCountMonitor,
     PeriodicExecutionTimeMonitor,
+    UnwantedHTTPCodesMonitor,
 )
 
 
 class CreateCustomFileReport(CreateFileReport):
-    template_paths = [Path(__file__).parent / "actions"]
+    """Overrides the default CreateFileReport to use a custom template."""
+
+    template_paths: ClassVar[list] = [Path(__file__).parent / "actions"]
 
 
 class SpiderCloseMonitorSuite(MonitorSuite):
     """
-    These monitors will only run once when the spider is closed. These will compare the "items_scraped_count" to SPIDERMON_MIN_ITEMS. If the items_scraped_count is less than SPIDERMON_MIN_ITEMS it will count as a failure. From testing the smallest site with a dpeth of 2 returned 10 items. Therefore we expect at least 10 items from a site. UnwantedHTTPCodesMonitor checks the count of the unwanted http codes (SPIDERMON_UNWANTED_HTTP_CODES). If any of those codes have a count higher than SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT it will count as a failure.
+    These monitors will only run once when the spider is closed.
 
-    A failed action will create a report which will be saved in the search_gov_crawler directory and sent to SPIDERMON_EMAIL_TO using Amazon SES.
+    These will compare the "items_scraped_count" to SPIDERMON_MIN_ITEMS. If the items_scraped_count is less
+    than SPIDERMON_MIN_ITEMS it will count as a failure. UnwantedHTTPCodesMonitor checks the count of the unwanted
+    http codes (SPIDERMON_UNWANTED_HTTP_CODES). If any of those codes have a count higher than
+    SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT it will count as a failure.
 
-    SPIDERMON_MIN_ITEMS, SPIDERMON_UNWANTED_HTTP_CODES, SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT, SPIDERMON_EMAIL_TO values are all defined in search_gov_crawler/search_gov_spiders/settings.py
+    A failed action will create a report which will be saved in the search_gov_crawler directory
+    and sent to SPIDERMON_EMAIL_TO using Amazon SES.
+
+    The run time at the end of the spider will fail if the spider has been running longer than
+    SPIDERMON_MAX_EXECUTION_TIME.
+
+    If the finish reason is not "finished" it will count as a failure. This is the default but can be mmodified
+    using SPIDERMON_EXPECTED_FINISH_REASONS.
+
+    Relevant settings are all defined in search_gov_crawler/search_gov_spiders/settings.py
     """
 
-    monitors = [ItemCountMonitor, UnwantedHTTPCodesMonitor]
-
-    monitors_failed_actions = [CreateCustomFileReport, SendSESEmail]
-
-
-class PeriodicMonitorSuite(MonitorSuite):
-    """
-    These monitors run once every SPIDERMON_TIME_INTERVAL (found in settings). The PeriodicItemCountMonitor looks that the "items_scraped_count" has increased by at least SPIDERMON_ITEM_COUNT_INCREASE each interval. The PeriodicExecutionTimeMonitor checks that the spider has not been running longer than SPIDERMON_MAX_EXECUTION_TIME.
-
-    A failed action will create a report which will be saved in the search_gov_crawler directory and sent to SPIDERMON_EMAIL_TO using Amazon SES.
-
-    SPIDERMON_TIME_INTERVAL,SPIDERMON_ITEM_COUNT_INCREASE and SPIDERMON_MAX_EXECUTION_TIME values are all defined in search_gov_crawler/search_gov_spiders/settings.py
-    """
-
-    monitors = [PeriodicItemCountMonitor, PeriodicExecutionTimeMonitor]
-
-    monitors_failed_actions = [CreateCustomFileReport, SendSESEmail]
+    monitors: ClassVar[list] = [
+        ItemCountMonitor,
+        UnwantedHTTPCodesMonitor,
+        PeriodicExecutionTimeMonitor,
+        FinishReasonMonitor,
+    ]
+    monitors_failed_actions: ClassVar[list] = [CreateCustomFileReport, SendSESEmail]
