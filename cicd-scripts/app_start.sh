a2c854698feefd905f937affd9d73fcd7a4275c1
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2025-05-14 14:31:29 -0400

    log to the same file

--- cicd-scripts/app_start.sh
+++ cicd-scripts/app_start.sh
@@ -13,11 +13,7 @@ sudo chown -R $(whoami) $LOG_FILE
 # Start sitemap monitor
 # run_sitemap_monitor.py file needs to be executed from search_gov_crawler because that's where the scrapy.cfg is
 SITEMAP_SCRIPT=run_sitemap_monitor.py
-SITEMAP_LOG_FILE=/var/log/scrapy_sitemap_monitor.log
 SITEMAP_DIR=/var/tmp/spider_sitemaps
-
-sudo touch $SITEMAP_LOG_FILE
-sudo chown -R $(whoami) $SITEMAP_LOG_FILE
 
 # Remove existing sitemap directory (if it exists)
 if [ -d "$SITEMAP_DIR" ]; then
@@ -27,7 +23,7 @@ fi
 # Recreate directory and set ownership
 sudo mkdir -p "$SITEMAP_DIR"
 sudo chown -R "$(whoami)" "$SITEMAP_DIR"
-nohup bash -c "source ./venv/bin/activate && cd ./search_gov_crawler && python $SITEMAP_SCRIPT" >> $SITEMAP_LOG_FILE 2>&1 &
+nohup bash -c "source ./venv/bin/activate && cd ./search_gov_crawler && python $SITEMAP_SCRIPT" >> $LOG_FILE 2>&1 &
 # Start scheduler
 nohup bash -c "source ./venv/bin/activate && ./venv/bin/python ./$START_SCRIPT" >> $LOG_FILE 2>&1 &
 
